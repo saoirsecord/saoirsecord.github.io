52db81c5d67eb56400a4472de390c6e97ea10cc0
suprstarrd <26027089+suprstarrd@users.noreply.github.com>
2025-11-09 18:19:05 -0500

    Update _middleware.ts

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -19,3 +19,38 @@ export const onRequestOptions: PagesFunction[] = [
     }
   }
 ]
+
+export const onRequestGet: PagesFunction [] = [
+  async (context) => {
+    let url = new URL(context.request.url);
+    if (url.pathname == '/api/v1/accounts/lookup') {
+      let search = decodeURIComponent(url.search);
+      if (search == '?acct=@saoirsecord@saoirsecord.com') {
+        await fetch('https://mastodon.social/api/v1/accounts/lookup', {
+          "headers": {
+            "Content-Type": "application/json;charset=utf-8"
+          }
+        })
+        .then((response) => res.json())
+        .then((data) => {
+          var body = data;
+          if (body && body['note']) body['note'] = body['note'] + ' https://justmytoots.com/@saoirsecord@saoirsecord.com'
+
+          return new Response(body, { "status": 200 });
+        })
+      } else {
+        try {
+          return await context.next();
+        } catch (err) {
+          return new Response(`${err.message}\n${err.stack}`, { status: 500 });
+        }
+      }
+    } else {
+      try {
+        return await context.next();
+      } catch (err) {
+        return new Response(`${err.message}\n${err.stack}`, { status: 500 });
+      }
+    }
+  }
+]
