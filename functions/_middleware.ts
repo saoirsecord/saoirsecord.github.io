a331117115e456de2cfb4ff8a34cc8c76ff511ad
suprstarrd <26027089+suprstarrd@users.noreply.github.com>
2025-11-09 22:37:00 -0500

    Update _middleware.ts

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -26,7 +26,7 @@ export const onRequestGet: PagesFunction [] = [
     if (url.pathname == '/api/v1/accounts/lookup') {
       let search = decodeURIComponent(url.search);
       if (search == '?acct=@saoirsecord@saoirsecord.com') {
-        return new Response(JSON.stringify({"note":"https://justmytoots.com/@saoirsecord@saoirsecord.com"}), { status: 200 });
+        return new Response(JSON.stringify({"note":"https://justmytoots.com/@saoirsecord@saoirsecord.com","fields":[]}), { status: 200 });
       } else {
         try {
           return await context.next();
